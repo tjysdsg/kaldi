853b2695298f417a9348e8ef81077ec591a0d2e8
tjysdsg <tjy1018543509@163.com>
2020-06-03 20:05:42 +0800

    Add build script

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+mkdir -p build
+pushd build
+cmake -DCMAKE_INSTALL_PREFIX=../installed -DCMAKE_CXX_FLAGS="-fPIC" -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER=/usr/bin/clang-8 -DCMAKE_CXX_COMPILER=/usr/bin/clang++-8 ..
+make -j8 install
+popd
