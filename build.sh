6487b268e4d756e20c93eb56d8f798dcc3b09da8
tjysdsg <tjy1018543509@163.com>
2020-06-04 21:30:18 +0800

    Update build script

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 mkdir -p build
 pushd build
-cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER=/usr/bin/clang-8 -DCMAKE_CXX_COMPILER=/usr/bin/clang++-8 ..
-make -j8
-sudo make install
+cmake -DCMAKE_INSTALL_PREFIX=../installed -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER=/usr/bin/clang-8 -DCMAKE_CXX_COMPILER=/usr/bin/clang++-8 ..
+make -j8 install
 popd
